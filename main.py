c7162407289deb107adbfca4041987ca1ac23a09
leonmen34 <leonmen34@protonmail.com>
2026-02-15 23:36:12 +0600

    Add gitignore

---
+++ main.py
@@ -0,0 +1,17 @@
+from collections import deque
+from system import systemInformation
+
+def checking_parentheses_for_correctness(inputData: str) -> bool:
+    return None
+
+if __name__=="__main__":
+
+    info = systemInformation._brackets
+    result = 1
+
+    user_input = input()
+
+    result = checking_parentheses_for_correctness(user_input)
+
+    print(result)
+    print(*info)
---
+++ system.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass
+class systemInformation:
+
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            # Если объекта еще нет, создаем его через базовый класс object
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    # Множество скобок
+    _brackets = [1,2,3]
